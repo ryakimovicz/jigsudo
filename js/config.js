c2a1e26af15a9a83bed0c533ed859c725d52cf22
Román Yakimovicz <romanyaki@hotmail.com>
2026-01-20 01:06:02 -0300

    Actualiza a 0.2.3

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  version: "v0.2.2", // Major.Minor.Patch
-  fechaUpdate: "2026-01-19",
+  version: "v0.2.3", // Major.Minor.Patch
+  fechaUpdate: "2026-01-20",
   autor: "Román Yakimovicz",
   debugMode: false, // Toggle to true to see logs and debug panels
 };
