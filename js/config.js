abb9255248203ac7b65ea2a4c0ee3f0a581e95de
Román Yakimovicz <romanyaki@hotmail.com>
2026-04-15 03:10:03 -0300

    Actualiza a 1.3.0

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  version: "1.2.2", // Season 1 Release
-  fechaUpdate: "2026-04-14",
+  version: "1.3.0", // Season 1 Release
+  fechaUpdate: "2026-04-15",
   author: "Román Yakimovicz",
   schemaVersion: 7.2,
-  debugMode: true, // Toggle to true to see logs
-  betaMode: true, // Toggle to true to enable "Ayuda" button
+  debugMode: false, // Toggle to true to see logs
+  betaMode: false, // Toggle to true to enable "Ayuda" button
 
   // Configuración de sincronización del servidor
   serverTimeSyncInterval: 300000, // 5 minutos
