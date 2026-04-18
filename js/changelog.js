69a2defb8a0218a303d581419684fcf687ab558f
Román Yakimovicz <romanyaki@hotmail.com>
2026-04-17 21:22:52 -0300

    actualiza changelog

--- js/changelog.js
+++ js/changelog.js
@@ -32,6 +32,18 @@ function renderChangelog() {
 
     const versions = [
         {
+            tag: "v1.3.5",
+            titleKey: "changelog_v135_title",
+            dateKey: "changelog_v135_date",
+            itemKeys: [
+                "changelog_v135_item1",
+                "changelog_v135_item2",
+                "changelog_v135_item3",
+                "changelog_v135_item4",
+                "changelog_v135_item5"
+            ]
+        },
+        {
             tag: "v1.3.4",
             titleKey: "changelog_v134_title",
             dateKey: "changelog_v134_date",
