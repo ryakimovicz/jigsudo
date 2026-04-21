f75a57a08fc35dfacc053bef803ae89bf665660d
Román Yakimovicz <romanyaki@hotmail.com>
2026-04-20 22:38:11 -0300

    Actualiza a v1.3.8

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  version: "1.3.7", // Sidebar Center & Lock Interaction
-  fechaUpdate: "2026-04-18",
+  version: "1.3.8", // Performance & Scoring Update
+  fechaUpdate: "2026-04-20",
   author: "Román Yakimovicz",
   schemaVersion: 7.2,
-  debugMode: true, // Toggle to true to see logs
-  betaMode: true, // Toggle to true to enable "Ayuda" button
+  debugMode: false, // Toggle to true to see logs
+  betaMode: false, // Toggle to true to enable "Ayuda" button
 
   // Configuración de sincronización del servidor
   serverTimeSyncInterval: 1800000, // 30 minutos
