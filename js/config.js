78ef7c67fc4bd6501d6caae98f64203c1ab86fb0
Román Yakimovicz <romanyaki@hotmail.com>
2026-04-22 06:53:48 -0300

    actualiza a v1.3.10

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  version: "1.3.9", // Performance & Scoring Update
-  fechaUpdate: "2026-04-21",
+  version: "1.3.10", // Performance & Scoring Update
+  fechaUpdate: "2026-04-22",
   author: "Román Yakimovicz",
   schemaVersion: 7.2,
   debugMode: true, // Toggle to true to see logs
