ecf4b28b14fe59fb9e17b127bb0819be83c79c46
Román Yakimovicz <romanyaki@hotmail.com>
2026-04-25 00:19:58 -0300

    Actualiza a v1.4.6

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  version: "1.4.5",
-  fechaUpdate: "2026-04-24",
+  version: "1.4.6",
+  fechaUpdate: "2026-04-25",
   author: "Román Yakimovicz",
   schemaVersion: 7.2,
-  debugMode: true, // Toggle to true to see logs
-  betaMode: true, // Toggle to true to enable "Ayuda" button
+  debugMode: false, // Toggle to true to see logs
+  betaMode: false, // Toggle to true to enable "Ayuda" button
 
   // Configuración de sincronización del servidor
   serverTimeSyncInterval: 1800000, // 30 minutos
