5ca68944519efc36e6ba059d2556828ac3c707d7
Román Yakimovicz <romanyaki@hotmail.com>
2026-04-28 15:49:40 -0300

    debugmode y betamode false

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ export const CONFIG = {
   fechaUpdate: "2026-04-27",
   author: "Román Yakimovicz",
   schemaVersion: 7.2,
-  debugMode: true, // Toggle to true to see logs
-  betaMode: true, // Toggle to true to enable "Ayuda" button
+  debugMode: false, // Toggle to true to see logs
+  betaMode: false, // Toggle to true to enable "Ayuda" button
   isDemo: true, // Itch.io Demo Mode
   demoDate: "2026-04-05", // Sunday, April 5, 2026
 
